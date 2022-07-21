08ea56227ada69e113fe333a1c10a9b02a32524c
blaquevybes <blaquevybes@gmail.com>
2022-07-21 16:17:20 -0500

    The basic structure of the car is ready. Currently working on advanceed features

---
+++ src/com/company/BasicFeatures.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class BasicFeatures {
+    String engineCapacity;
+    String color;
+    String doors;
+    String headlights;
+    String regNo;
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+        // Write your code here
+        BasicFeatures car = new BasicFeatures();
+
+    }
+}
